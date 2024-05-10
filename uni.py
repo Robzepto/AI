04dbd236d20cf0755ce70bc0fbcd6e16e0877577
Robzepto <narayanraoriot@gmail.com>
2024-05-10 08:10:05 +0530

    Update uni.py

--- uni.py
+++ uni.py
@@ -1,36 +1,19 @@
-def is_variable(term):
-    return isinstance(term, str) and term[0].isupper()
-def unify_lists(x, y, theta):
-    if len(x) != len(y):
-        return None
-    for xi, yi in zip(x, y):
-        theta = unify(xi, yi, theta)
-        if theta is None:
-            return None
-    return theta
-    
-def unify_var(var, x, theta):
-    if var in theta:
-        return unify(theta[var], x, theta)
-    elif x in theta:
-        return unify(var, theta[x], theta)
-    else:
-        theta[var] = x
-        return theta
-        
-def unify(x, y, theta):
+def unify(x, y, theta={}):
     if theta is None:
         return None
     elif x == y:
         return theta
-    elif is_variable(x):
-        return unify_var(x, y, theta)
-    elif is_variable(y):
-        return unify_var(y, x, theta)
+    elif isinstance(x, str) and x[0].isupper():
+        return {**theta, x: y} if x not in theta else unify(theta[x], y, theta)
+    elif isinstance(y, str) and y[0].isupper():
+        return {**theta, y: x} if y not in theta else unify(x, theta[y], theta)
     elif isinstance(x, list) and isinstance(y, list):
-        return unify_lists(x, y, theta)
+        return None if len(x) != len(y) else unify_lists(x, y, theta)
     else:
         return None
 
-theta = unify(['father', 'X', 'Y','Z'], ['father', 'John', 'Alice','Bob'], {})
+def unify_lists(x, y, theta={}):
+    return theta if not x else unify_lists(x[1:], y[1:], unify(x[0], y[0], theta))
+
+theta = unify(['father', 'X', 'Y','Z'], ['father', 'John', 'Alice','Bob'])
 print("Substitutions:", theta)
